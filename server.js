94576ad6bfe551b25c5036560906d000bb93ebfd
Ahmad Kayyali <kayyali18@gmail.com>
2021-05-18 22:43:29 -0400

    Add mongoose to server

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ import express from "express";
 
 import userRouter from "./routers/user.js";
 import data from "./data.js";
+import mongoose from "mongoose";
 
 const app = express();
+// Setup mongodb_url
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
 const PORT = process.env.PORT || 8080;
 
+/** GET */
 app.get("/api/v1/products", (req, res) => {
   res.status(200).send(data.products);
 });
@@ -21,8 +30,14 @@ app.get("/", (req, res) => {
   res.send(`Server is ready and running on port: ${PORT} `);
 });
 
+/** Routing */
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+// User Router
+app.use("/api/v1/users", userRouter);
+
 app.listen(PORT, () => {
   console.log("Served at http://localhost:8080");
 });
-
-app.use("/api/v1/users", userRouter);
